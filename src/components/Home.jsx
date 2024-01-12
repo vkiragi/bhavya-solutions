4ca2f4e33332a1878cd7dc807304d3c7549ab81c
vkiragi <varunkiragi@icloud.com>
2024-01-11 19:39:42 -0800

    added mobile nav functionalities

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { FaEnvelope } from "react-icons/fa";
-import profilePic from "/Users/varunkiragi/Documents/Workspace/bhavya-solutions/src/assets/books.jpeg";
+import profilePic from "/Users/varunkiragi/Documents/Workspace/bhavya-solutions/src/assets/books.jpeg"; // Modify the path as needed
 
 const Home = () => {
   return (
     <div id="home" className="w-full h-screen bg-[#F5F5DC]">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <div className="flex space-x-10">
-          <div className="w-1/2 pr-10">
-            <p className="text-emerald-500 mb-2">Hi, I am Mamta Jha! </p>
+      <div className="max-w-[1000px] w-full mx-auto px-8 flex flex-col justify-center h-full sm:text-left text-center">
+        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-10 items-center sm:items-start">
+          <div className="w-full sm:w-1/2">
+            <p className="text-emerald-500 mb-2">Hi, I am Mamta Jha!</p>
             <div className="mb-2">
-              <h1 className="text-4xl sm:text-7xl font-bold text-[#2F4F4F] ml-[-5px]">
+              <h1 className="text-3xl sm:text-4xl lg:text-7xl font-bold text-[#2F4F4F]">
                 Bhavya Solutions
               </h1>
-              <h2 className="text-4xl sm:text-6xl font-bold text-[#3CB371] ml-[-5px]">
+              <h2 className="text-2xl sm:text-3xl lg:text-6xl font-bold text-[#3CB371]">
                 Editorial Services
               </h2>
             </div>
-            <p className="text-[#2F4F4F] mb-2 max-w-[700px] mr-6">
+            <p className="text-[#2F4F4F] mb-2 max-w-[400px] mx-auto sm:max-w-[500px] lg:max-w-[600px]">
               I provide editing and indexing services for thoughtful texts in a
               wide variety of subjects.
             </p>
-
-            <div className="flex-col justify-start items-start">
+            <div className="flex flex-col justify-start items-center sm:items-start">
               <a href="mailto:mamta@bhavyasolutions.com">
                 <button className="text-slate-600 group border-slate-600 border-2 rounded-lg px-6 py-3 my-4 flex items-center hover:bg-[#3CB371] hover:border-[#3CB371] transition duration-150">
                   Get in Touch
@@ -32,11 +31,11 @@ const Home = () => {
               </a>
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="w-full sm:w-1/2 lg:w-3/4 xl:w-4/5">
             <img
               src={profilePic}
               alt="Profile"
-              className="profile-pic w-auto h-full mx-auto rounded-lg self-center"
+              className="w-auto h-48 sm:h-full lg:h-auto lg:max-h-[600px] mx-auto rounded-lg self-center"
             />
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,59 +8,155 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div
-      className="fixed w-full h-[50px] flex justify-center items-center px-4 bg-[rgba(23,62,67,0.8)] text-[#FFFFFF]"
-    >
-      {/* menu */}
-      <ul className="hidden md:flex">
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+    <div className="fixed w-full h-[50px] flex items-center justify-center bg-[rgba(23,62,67,0.8)] text-[#FFFFFF]">
+      {/* Desktop menu */}
+      <ul className="hidden md:flex justify-center space-x-4">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="home" spy={true} smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="about" spy={true} smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="indexing-services" spy={true} smooth={true} duration={500}>
             Indexing Services
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="editing-services" spy={true} smooth={true} duration={500}>
             Editing Services
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="selected-clients" spy={true} smooth={true} duration={500}>
             Selected Clients
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="testimonials" spy={true} smooth={true} duration={500}>
             Testimonials
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="work-samples" spy={true} smooth={true} duration={500}>
             Work Samples
           </Link>
         </li>
-        <li className="hover:text-[#3CB371] hover:duration-300 duration-300">
+        <li className="hover:text-[#3CB371] duration-300">
           <Link to="contact" spy={true} smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
-      {/* hamburger */}
-      <div onClick={handleClick} className="md:hidden z-10">
+
+      {/* Hamburger */}
+      <div
+        onClick={handleClick}
+        className="md:hidden z-10 cursor-pointer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
-      {/* mobile menu */}
-      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#191970] flex flex-col justify-center items-center"}>
-        {/* Add your mobile menu items here */}
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[rgba(23,62,67,0.8)] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="indexing-services"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Indexing Services
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="editing-services"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Editing Services
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="selected-clients"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Selected Clients
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="testimonials"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Testimonials
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="work-samples"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Work Samples
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setNav(false)}
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
     </div>
   );
